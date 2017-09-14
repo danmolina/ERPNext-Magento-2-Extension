95364593744d0d7f96e4f756357ffe765717acd6
Dan Michael Molina <danmichaelmolina@gmail.com>
2017-09-14 21:21:48 +0800

    sideci testing
    sideci testing

--- Erpnext/registration.php
+++ Erpnext/registration.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * @author Amasty Team
- * @copyright Copyright (c) 2015 Amasty (http://www.amasty.com)
- * @package Amasty_HelloWorld
- */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Redscript_Erpnext', 
